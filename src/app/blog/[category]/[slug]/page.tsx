9c0f7b64c41d2365014105ef9fdbc7011f77a413
sejarparvez <sejarparvez@yahoo.com>
2023-11-11 19:53:02 +0600

    fixed metadata

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -5,6 +5,8 @@ interface PageProps {
   params: { slug: string; category: string };
 }
 
+const siteurl = process.env.NEXT_PUBLIC_SITE_URL;
+
 export async function generateMetadata(
   { params }: PageProps,
   parent: ResolvingMetadata
@@ -12,31 +14,53 @@ export async function generateMetadata(
   const { category, slug } = params;
 
   try {
-    const response = await fetch(`/api/${category}/${slug}`);
-    if (response.ok) {
-    }
+    const response = await fetch(`${siteurl}/api/${category}/${slug}`);
+
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error(
+        `Network response was not ok (${response.status} ${response.statusText})`
+      );
     }
 
     const post = await response.json();
 
-    // optionally access and extend (rather than replace) parent metadata
+    // Optionally access and extend (rather than replace) parent metadata
     const previousImages = (await parent).openGraph?.images || [];
 
     return {
-      title: post.title || "Blog Post", // Set a default title if post.title is null or undefined
+      title: post.title || "Blog Post",
+      description: post.content.slice(0, 100) || "This is a blog post",
       openGraph: {
+        title: post.title || "Blog Post",
+        description: post.content.slice(0, 100) || "This is a blog post",
+        type: "article",
+        url: `${siteurl}/category/${category}/${slug}`,
+        authors: post.author.name,
+        publishedTime: post.createdAt,
+        modifiedTime: post.updatedAt,
+        section: post.category,
         images: [post.coverImage, ...previousImages],
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: post.title,
       },
     };
   } catch (error) {
     console.error("Error fetching post:", error);
-    // Handle the error (set a default title or handle it in a way that fits your application)
+
+    // Handle specific error cases or set a default title based on the error
+    let defaultTitle = "Default Title";
+    if (error instanceof Error) {
+      if (error.message.includes("not ok")) {
+        defaultTitle = "Network Error";
+      }
+    }
+
     return {
-      title: "Default Title",
+      title: "Blog Post",
       openGraph: {
-        images: [], // Provide default images array if necessary
+        images: [],
       },
     };
   }
