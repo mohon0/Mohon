a9ac0fc6793632285d378a097c1454c81c644789
sejarparvez <sejarparvez@yahoo.com>
2023-11-22 10:22:14 +0600

    minor improvment

--- src/app/api/apply/route.ts
+++ src/app/api/apply/route.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 const secret = process.env.NEXTAUTH_SECRET;
@@ -8,19 +7,8 @@ const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    const token = await getToken({ req, secret });
-    const userId = token?.sub;
-    const userEmail = token?.email;
-
-    if (!userId) {
-      return new NextResponse("User not logged in or userId/userEmail missing");
-    }
-
-    if (userEmail === admin) {
-      const application = await prisma.application.findMany({});
-      return new NextResponse(JSON.stringify({ application }));
-    }
-    return new NextResponse("You are not allowed to access this");
+    const application = await prisma.application.findMany({});
+    return new NextResponse(JSON.stringify({ application }));
   } catch (error) {
     return new NextResponse("Error while getting application");
   }
