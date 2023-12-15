e0c142f908086bb92c272ec165ddae9cab02b934
sejarparvez <sejarparvez@yahoo.com>
2023-12-15 19:00:06 +0600

    aded grant token

--- src/app/api/bkash/payment/create/route.ts
+++ src/app/api/bkash/payment/create/route.ts
@@ -4,9 +4,37 @@ export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const { amount, orderId } = await req.json();
 
+    try {
+      const response = await fetch(`${process.env.bkash_grant_token_url}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          username: process.env.bkash_username || "", // Provide a default value or handle the undefined case
+          password: process.env.bkash_password || "", // Provide a default value or handle the undefined case
+        },
+        body: JSON.stringify({
+          app_key: process.env.bkash_api_key,
+          app_secret: process.env.bkash_secret_key,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to obtain bKash token");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+      console.error("Error obtaining bKash token:", error);
+      return new NextResponse("Error obtaining bKash token", { status: 500 });
+    }
+
     console.log(amount, orderId);
-    return new NextResponse("Api is working", { status: 200 });
+    return new NextResponse("API is working", { status: 200 });
   } catch (error) {
+    console.error("Error in API route:", error);
     return new NextResponse("Error", { status: 400 });
   }
 }
