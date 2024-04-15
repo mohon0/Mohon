410aafcf69cd5f2224a5dea9c104c21d41baf35f
sejarparvez <sejarparvez@yahoo.com>
2024-04-15 07:37:58 +0600

    improve about me page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -9,23 +9,47 @@ import img1 from "@/images/hero/img1.jpeg";
 import { Metadata } from "next";
 import Image from "next/image";
 
+const siteurl = process.env.NEXT_PUBLIC_SITE_URL;
+
 export const metadata: Metadata = {
   title: "About Me",
+  description: "All you need to know about me",
+  alternates: {
+    canonical: `/blog/category/about`,
+  },
+  openGraph: {
+    title: "About Me",
+    description: "All you need to know about me",
+    type: "profile",
+    url: `${siteurl}/about`,
+
+    images: [
+      {
+        url: process.env.NEXT_PUBLIC_SITE_URL + img1.src,
+        alt: "profile image",
+      },
+    ],
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "About Me",
+    description: "All you need to know about me",
+  },
 };
 
 export default function AboutMe() {
   return (
-    <div className="">
-      <div className="flex items-center justify-center">
-        <Button size="lg">About Me</Button>
-      </div>
+    <>
       <div className="mx-1 mt-10 flex flex-col-reverse gap-4 md:mx-4 md:flex-row lg:mx-20 lg:gap-16">
-        <AboutMeCard />
+        <div className="md:w-11/12 lg:w-9/12">
+          <AboutMeCard />
+        </div>
         <div className="flex flex-col gap-10">
           <Image
             src={img1}
             alt=""
-            className="rounded-xl object-cover lg:h-80"
+            className="rounded-xl object-cover md:h-60 lg:h-80"
             priority
           />
           <a
@@ -44,6 +68,6 @@ export default function AboutMe() {
       <Projects />
       <Cta />
       <Contact />
-    </div>
+    </>
   );
 }
--- src/app/(pages)/blog/category/[category]/[slug]/page.tsx
+++ src/app/(pages)/blog/category/[category]/[slug]/page.tsx
@@ -16,11 +16,8 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   const { category, slug } = params;
   const siteurl = process.env.NEXT_PUBLIC_SITE_URL;
-
   const response = await axios.get(`${siteurl}/api/${category}/${slug}`);
-
   const cleanedContent = stripHtmlTags(response.data.content);
-
   const dynamicDescription = cleanedContent.substring(0, 150);
 
   return {
