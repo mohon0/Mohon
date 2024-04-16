d26e14b3feb9daa15741c3e1e2c1cad9b88f76b7
sejarparvez <sejarparvez@yahoo.com>
2024-04-16 11:12:39 +0600

    improve navbar

--- src/components/layout/BestComputer.tsx
+++ src/components/layout/BestComputer.tsx
@@ -21,7 +21,7 @@ export function BestComputer() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <span className="relative overflow-hidden bg-background px-6 py-2 font-semibold duration-300 hover:bg-primary hover:shadow-cyanshadow">
+        <span className="relative overflow-hidden bg-background px-4 py-1.5 duration-300 hover:bg-primary hover:shadow-cyanshadow">
           <span className="absolute left-0 top-0 block h-0.5 w-full animate-animate1 bg-gradient-midnight"></span>
           <span className="absolute right-0 block h-full w-0.5 animate-animate2 bg-gradient-midnight"></span>
           <span className="absolute bottom-0 right-0 block h-0.5 w-full animate-animate3 bg-gradient-midnight "></span>
